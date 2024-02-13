d3d2770a5d865ac58e702208439a5c960fe57bf7
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2024-02-13 20:54:47 -0300

    feat: add middleware to match the router /dashboard, implements redirect when the user does not have access

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,16 +3,20 @@ import { NextRequest, NextResponse } from 'next/server'
 
 export async function middleware(req: NextRequest) {
   const cookie = cookies().get('Auth')?.value
-
-  console.log(cookie)
-
-  if (cookie) {
+  const pathName = req.nextUrl.pathname
+  const splittedPath = pathName.substring(1).split('/')
+  const isDashboard = splittedPath[1] === 'team-members'
+  if (cookie && pathName === '/') {
     return NextResponse.redirect(new URL('/dashboard/team-members', req.url))
+  }
+
+  if (!cookie && isDashboard) {
+    return NextResponse.redirect(new URL('/', req.url))
   }
 
   return NextResponse.next()
 }
 
 export const config = {
-  matcher: '/',
+  matcher: ['/', '/dashboard/team-members'],
 }
