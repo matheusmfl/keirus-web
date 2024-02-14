0022a69de5b57c5174ae1a9b4be4c2d84eebfa8c
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2024-02-13 21:48:24 -0300

    fix: Add pre-fix Next_Public to the environments

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod'
 
 const envSchema = z.object({
-  SERVER_API_URL: z.string().url(),
+  NEXT_PUBLIC_SERVER_API_URL: z.string().url(),
 })
 
 export const env = envSchema.parse(process.env)
