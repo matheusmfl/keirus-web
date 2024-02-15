9d0ce2af9384e0fdd3b3247a8d110d5c40b95b2b
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2024-02-14 22:31:15 -0300

    feat: add function logout ( Remove cookie auth )

--- src/app/dashboard/components/AsideNavMenu.tsx
+++ src/app/dashboard/components/AsideNavMenu.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import logo from '@/assets/logo.svg'
 import { NavigationContainer } from './NavigationContainer'
+import { LogoutComponent } from './Logout'
 
 const mockOptions = [
   { label: 'Courses', path: '/courses' },
@@ -16,14 +17,16 @@ export function AsideNavMenu() {
   }
 
   return (
-    <div className="w-[218px] min-h-screen fixed left-0 top-0 bg-[#061235] pt-[52px] pb-[18px] flex flex-col items-center">
+    <div className="w-[218px] min-h-screen fixed left-0 top-0 bg-[#061235] pt-[52px] pb-[18px] flex flex-col items-center justify-between">
       <div className="flex flex-col gap-6">
         <div className="px-10">
           <Image src={logo} alt="Keirus logo" />
         </div>
 
         <NavigationContainer options={mockOptions} isActive={setIsActive} />
-        <nav></nav>
+      </div>
+      <div className="h-full flex items-end justify-end">
+        <LogoutComponent />
       </div>
     </div>
   )
